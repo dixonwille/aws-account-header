81fa08e5de03fce99cd0ba5bbc0e19e2840fee82
Neil Whitworth <Neil.Whitworth@rivieratravel.co.uk>
2021-05-27 21:05:06 +0100

    add support for aws console redesign. Get account ID from ConsoleNavService

--- aws.js
+++ aws.js
@@ -2,17 +2,36 @@ function onError(error) {
   console.log(`Error: ${error}`);
 }
 
+function getCurrentAWSAccountID() {
+  if ( window.wrappedJSObject.ConsoleNavService && window.wrappedJSObject.ConsoleNavService.AccountInfo) {
+	const accInfo = window.wrappedJSObject.ConsoleNavService.AccountInfo;
+    return accInfo.userAccountNumber;
+  }
+  const ele = document.getElementById("awsc-login-display-name-account")
+  if (ele) {
+    return ele.textContent;
+  }
+  return null;
+}
+
+function normaliseAccountID(acct) {
+  return acct.replace(/-/ig, ''); 	
+}
+
 function onGot(item) {
-  var acct = document.getElementById("awsc-login-display-name-account")
-    .textContent;
-  if (item.hasOwnProperty(acct)) {
-    document
-      .querySelectorAll(
-        "body #awsgnav #nav-menubar, body #awsgnav #nav-menubar .nav-menu, #nav-menu-right"
-      )
-      .forEach(function(ele) {
-        ele.style.backgroundColor = item[acct];
-      });
+  var acct = getCurrentAWSAccountID()
+  if (acct) {
+    acct = normaliseAccountID(acct);
+
+    if (item.hasOwnProperty(acct)) {
+      document
+        .querySelectorAll(
+          "body #awsgnav #nav-menubar, body #awsgnav #nav-menubar .nav-menu, #nav-menu-right, #awsc-nav-header"
+        )
+        .forEach(function(ele) {
+          ele.style.backgroundColor = item[acct];
+        });
+    }
   }
 }
 
